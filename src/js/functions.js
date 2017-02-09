88d68757e38ae21b4fd0005b2d7038456352a6a0
maxime.brugel <maxime.brugel@gmail.com>
2017-02-09 12:06:44 +0100

    Commit du 09/02 du site d'asministration : - Création des formulaires de parking et de place, mais le JS n'est pas encore fonctionnel. Il faut également modifier la présentation du formulaire et ajouter des champs éventuellement (à voir avec la partie JEE)

---
+++ src/js/functions.js
@@ -0,0 +1,17 @@
+/**
+ * functions.js					09/02/2017
+ */
+
+var bouton_valider_parking = $('.button-valider-parking');
+var bouton_valider_place = $('.button-valider-place');
+
+/* Évènement click création d'un parking */
+bouton_valider_parking.click(function(ev) {
+	  alert("Création parking");
+	  // TODO création d'un parking
+});
+
+bouton_valider_place.click(function(ev) {
+	alert("Création parking");
+	// TODO création d'un place
+});
